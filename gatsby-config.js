a2a496c283fbfe19412152b28dcb9bb82bf22cbe
Clarice Bouwer <clarice@simply.co.za>
2022-05-25 02:06:42 +0400

    personalize configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: 'Curious Programmer',
+    description:
+      'A curious place for a curious mind. I share my thoughts. I share my ideas. I share my what I think is cool.',
+    keywords: 'software development, software, programming',
+    url: 'https://curiousprogrammer.dev',
+    // This duplicated gem is used for gatsby-plugin-advanced-sitemap
+    // and gatsby-plugin-robots-txt
+    // https://www.gatsbyjs.com/plugins/gatsby-plugin-advanced-sitemap/?=sitemap
+    // https://www.gatsbyjs.com/plugins/gatsby-plugin-robots-txt/?=Robots.txt
+    siteUrl: 'https://curiousprogrammer.dev',
+    lang: 'en-US',
+    brand: '#f0ff7b',
+    author: {
+      name: 'Clarice Bouwer',
+      url: 'https://clarice.bouwer.dev',
+    },
+    social: {
+      twitter: 'cbillowes',
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,16 +38,16 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#f0ff7b`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        // theme_color: `#f0ff7b`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
