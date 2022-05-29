86a38339086a393716ca662fa9dd1bf123a1cc3e
Clarice Bouwer <clarice@simply.co.za>
2022-05-29 07:06:12 +0400

    new typewriter effect

--- src/components/Typewriter.js
+++ src/components/Typewriter.js
@@ -1,36 +1,21 @@
-// https://codesandbox.io/s/react-text-typing-effect-in-infinite-loop-hooks-rycur?file=/src/index.js:439-680
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Typist from "react-typist"
+import TypewriterEffect from 'typewriter-effect';
 
-const Typewriter = ({ prefix, count, setCount, phrases, children }) => {
-  return <></>;
-  // if (phrases && phrases.length > 0) {
-  //   return count ? (
-  //     <Typist avgTypingDelay={120} onTypingDone={() => setCount(0)}>
-  //       {`${prefix} `}
-  //       {phrases.map((phrase, index) => (
-  //         <span key={index}>
-  //           <span>
-  //             <span className="font-semibold">{phrase}</span>
-  //           </span>
-  //           <Typist.Backspace count={phrase.length} delay={800} />
-  //         </span>
-  //       ))}
-  //     </Typist>
-  //   ) : (
-  //     <></>
-  //   )
-  // }
-  // return children
+const Typewriter = ({ phrases }) => {
+  return (
+    <TypewriterEffect
+      options={{
+        strings: phrases,
+        autoStart: true,
+        loop: true,
+      }}
+    />
+  );
 };
 
 Typewriter.propTypes = {
-  prefix: PropTypes.string,
-  count: PropTypes.number.isRequired,
-  setCount: PropTypes.func.isRequired,
-  phrases: PropTypes.array.isRequired,
-  children: PropTypes.node.isRequired,
+  phrases: PropTypes.array,
 };
 
 export default Typewriter;
