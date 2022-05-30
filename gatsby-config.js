f2b96fc374bcd7b27adf10fb884f46170d1ca81e
Clarice Bouwer <clarice@simply.co.za>
2022-05-30 06:33:40 +0400

    configure gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,15 +46,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#f0ff7b`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#f0ff7b`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        name: siteMetadata.title,
+        short_name: siteMetadata.title,
+        start_url: '.',
+        background_color: '#171E29',
+        theme_color: '#f0ff7b',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
       },
     },
     {
