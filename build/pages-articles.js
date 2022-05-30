63015a3a4fc1342f2fb2825dbccbc9acde995252
Clarice Bouwer <clarice@simply.co.za>
2022-05-30 06:49:40 +0400

    build articles: order by date

--- build/pages-articles.js
+++ build/pages-articles.js
@@ -8,7 +8,7 @@ const landingPageSlug = '/';
 const query = async (graphql) => {
   return await graphql(`
     query ArticlesBuildQuery {
-      allMarkdownRemark(sort: { order: ASC, fields: fields___number }) {
+      allMarkdownRemark(sort: { order: ASC, fields: fields___date }) {
         edges {
           node {
             html
