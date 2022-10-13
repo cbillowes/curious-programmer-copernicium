350defe917c929192c6e9d814f16dc0ace1f2f47
Clarice Bouwer <cbillowes@gmail.com>
2022-10-13 18:55:50 +0400

    chapters page

--- src/templates/chapters.js
+++ src/templates/chapters.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import { Layout } from '../components/Layout';
-import Tags from '../components/Tags';
 import { getKeywords } from '../common/seo';
 import CommentSystem from '../components/CommentSystem';
 import { MdOutlineSchool } from 'react-icons/md';
+import Anchor from '../components/Anchor';
 
 export const query = graphql`
   query ScribblesTemplateQuery($slug: String!) {
@@ -45,7 +45,7 @@ export const query = graphql`
 
 const ChaptersTemplate = ({ data, pageContext }) => {
   const { markdownRemark, site } = data;
-  const { page, modified, total } = pageContext;
+  const { page, modified, total, next, previous } = pageContext;
   const { excerpt, html, timeToRead, fields, frontmatter } = markdownRemark;
   const { title, description, url } = site.siteMetadata;
   const { title: courseTitle, tags, parent, abstract } = frontmatter;
@@ -63,19 +63,30 @@ const ChaptersTemplate = ({ data, pageContext }) => {
         route: fields.slug,
       }}
     >
-      <div id="article" className="pt-14 px-4 pb-24">
+      <div id="article" className="pt-14 px-4 pb-24 max-w-screen-lg mx-auto">
+        <div className="uppercase text-center my-3 opacity-40"></div>
+        <div className="uppercase text-center my-3 opacity-40 flex justify-between">
+          <Anchor to={previous} className={previous ? '' : 'invisible'}>
+            &larr; Previous
+          </Anchor>
+          <div>
+            <Anchor to="/courses">
+              <MdOutlineSchool
+                className="inline-block mr-2 bg-color-neutral p-2 text-4xl rounded"
+                alt="Courses"
+              />
+              Courses
+            </Anchor>
+           {' '} / {' '}
+            <Anchor to={parent}>Table of Contents</Anchor>
+          </div>
+          <Anchor to={next} className={next ? '' : 'invisible'}>
+            Next &rarr;
+          </Anchor>
+        </div>
         <h1 className="text-center font-bold px-4 md:px-10 max-w-screen-xl mx-auto">
           {courseTitle}
         </h1>
-        <div className="uppercase text-center my-3 opacity-40">
-          <Link to="/courses">
-            <MdOutlineSchool
-              className="inline-block mr-2 bg-color-neutral p-2 text-4xl rounded"
-              alt="Courses"
-            />
-          </Link>
-          {fields.type} &middot; <Link to={parent}>Back</Link>
-        </div>
         <div className="text-center text-neutral">
           Estimated {timeToRead} minute read &middot; Page {parseInt(page, 10)}{' '}
           out of {total} &middot; Last modified:{' '}
@@ -85,13 +96,9 @@ const ChaptersTemplate = ({ data, pageContext }) => {
             month: 'long',
             day: 'numeric',
           })}
-          <p>
-            {abstract}
-          </p>
+          <p>{abstract}</p>
         </div>
-        <div className="text-center">
-          <Tags tags={tags} redirect={true} isButton={true} />
-        </div>
+        <hr />
         <div
           className="content max-w-3xl mx-auto mt-8"
           // eslint-disable-next-line react/no-danger
